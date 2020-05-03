f7b4a726ea58f8c93fa9c2b2372c0033d2634def
rizkyputra531 <rizkyputra531.rp@gmail.com>
2020-05-03 12:32:58 +0800

    Memanggil Method dari Banyak Class di Package Lain

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 import koneksi.Database2;
+import model.Dosen;
+import model.Mahasiswa;
+import model.MataKuliah;
 
 public class Main{
  public static void main(String [] args){
   System.out.println("Ini program main");
   Database2.hubungkan();
+  Dosen.tampilaninfo();
+  Mahasiswa.tampilaninfo();
+  MataKuliah.tampilaninfo();
  }
 }
---
+++ model/Mahasiswa.java
@@ -0,0 +1,12 @@
+package model;
+
+public class Mahasiswa{
+	public static void tampilaninfo(){
+		System.out.println();
+		System.out.println("== Data Mahasiswa ======================");
+		System.out.println("NPM : 18630277");
+		System.out.println("Nama : Muhammad Rizky Syahputra");
+		System.out.println("========================================");
+	}
+}
+		
